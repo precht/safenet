d1d4334bbb7186c3f7104061730fde21a30a0c23
Jakub Precht <mr.loku@gmail.com>
2017-09-14 03:54:02 +0200

    wix small fix

--- wix/build.bat
+++ wix/build.bat
@@ -1,3 +1,4 @@
+windeployqt.exe --qmldir ..\client\safenet ..\client\build\release\safenet.exe
 if exist Safenet.msi del Safenet.msi
 if exist Safenet.wixobj del Safenet.wixobj
 if exist Safenet.wixpdb del Safenet.wixpdb
